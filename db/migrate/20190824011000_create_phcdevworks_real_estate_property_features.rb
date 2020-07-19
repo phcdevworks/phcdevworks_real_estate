26cef9c27d80f6a1cf5ccde38169478eb002b685
BradPotts <brad.potts@phcnet.co>
2020-07-19 03:10:06 +0000

    cleaned up db files.

--- db/migrate/20190824011000_create_phcdevworks_real_estate_property_features.rb
+++ db/migrate/20190824011000_create_phcdevworks_real_estate_property_features.rb
@@ -1,15 +1,17 @@
 class CreatePhcdevworksRealEstatePropertyFeatures < ActiveRecord::Migration[6.0]
   def change
     create_table :phcdevworks_real_estate_property_features do |t|
-    
+
       t.string :feature_name
+
+      t.string :optimization_id
 
       t.string :slug
       t.string :user_id
       t.string :org_id
 
       t.timestamps
-    
+
     end
   end
 end
--- db/migrate/20191024232111_create_phcdevworks_real_estate_property_features_listings.rb
+++ db/migrate/20191024232111_create_phcdevworks_real_estate_property_features_listings.rb
@@ -1,8 +1,10 @@
 class CreatePhcdevworksRealEstatePropertyFeaturesListings < ActiveRecord::Migration[6.0]
   def change
     create_table :phcdevworks_real_estate_features_listings do |t|
+
       t.integer :feature_id
       t.integer :listing_id
+
     end
   end
 end
